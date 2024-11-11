31680ec40981cfb935bef1c0b4f80c48e4c9cf37
dparejaUniandes <d.pareja@uniandes.edu.co>
2024-11-11 08:17:39 -0500

    created files from other repository

---
+++ src/application.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+import funtions as f
+
+application = Flask(__name__)
+data = f.load_file('./src/heroes.csv')
+
+@application.route("/")
+def index():
+    return jsonify(data)
+
+@application.route("/<string:id>")
+def heroe(id):
+    return jsonify(data[id])
+
+if __name__ == "__main__":
+    application.run(host = "0.0.0.0", port = 5000, debug = True)
